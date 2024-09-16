4de64fe5507923eb297541bf6fa27c1c863e154b
starwook <star.wookk@gmail.com>
2024-09-16 13:39:56 +0900

    style: git 캐시 삭제 및 재적용

--- src/main/java/com/server/computer_science/question/common/ProblemLevel.java
+++
@@ -1,6 +0,0 @@
-package com.server.computer_science.question.common;
-
-public enum ProblemLevel {
-
-    LOW,MEDIUM,HIGH
-}
--- src/main/java/com/server/computer_science/question/normal_question/repository/NormalQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.server.computer_science.question.normal_question.repository;
-
-import com.server.computer_science.question.normal_question.domain.NormalQuestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NormalQuestionRepository extends JpaRepository<NormalQuestion,Long> {
-}
