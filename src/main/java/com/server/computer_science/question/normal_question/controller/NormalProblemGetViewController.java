23a724ffcb8747af6bea0233cae87f0c3f8d74a7
starwook <star.wookk@gmail.com>
2024-10-08 19:17:02 +0900

    feat: View - 주관식 문제 채점 기능 구현

--- src/main/java/com/server/computer_science/question/normal_question/controller/NormalProblemGetViewController.java
+++ src/main/java/com/server/computer_science/question/normal_question/controller/NormalProblemGetViewController.java
@@ -1,9 +1,6 @@
 package com.server.computer_science.question.normal_question.controller;
 
-import com.server.computer_science.question.common.QuestionCategory;
-import com.server.computer_science.question.common.QuestionLevel;
 import com.server.computer_science.question.normal_question.dto.request.RequestGetNormalQuestionsDto;
-import com.server.computer_science.question.normal_question.dto.response.ResponseNormalQuestionDto;
 import com.server.computer_science.question.normal_question.service.NormalQuestionGetService;
 import com.server.computer_science.question.normal_question.service.QuestionSelectorService;
 import lombok.RequiredArgsConstructor;
@@ -11,9 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -34,10 +29,11 @@ public class NormalProblemGetViewController {
     @GetMapping("/question/normal")
     public String getNormalQuestions(@RequestParam(required = false) List<String> levels,
                                      @RequestParam(required = false) List<String> categories,
-                                     @RequestParam(required = false) Boolean MultipleChoice,
+                                     @RequestParam(required = false) Boolean multipleChoice,
                                      Model model) {
         model.addAttribute("questions", normalQuestionGetService.getClassifiedNormalQuestions(RequestGetNormalQuestionsDto.fromString(categories, levels)));
-        model.addAttribute("multipleChoice",MultipleChoice);
+        model.addAttribute("multipleChoice", multipleChoice);
+        System.out.println(multipleChoice);
         return "normal-question"; // 문제를 보여줄 페이지의 이름
     }
 
