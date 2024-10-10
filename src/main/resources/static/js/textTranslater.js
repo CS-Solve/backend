511368d1d54b880793c0c9ec48a6dd1c5db37589
starwook <star.wookk@gmail.com>
2024-10-11 03:40:08 +0900

    feat: View - 문제, 해설, 난이도 수정 기능 구현

--- src/main/resources/static/js/textTranslater.js
+++ src/main/resources/static/js/textTranslater.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const descriptions = document.querySelectorAll('.descriptionText, #descriptionText, .questionItem');
+    const descriptions = document.querySelectorAll('.descriptionText, #descriptionText, .questionTitle');
     descriptions.forEach(function(element) {
         element.innerHTML = element.textContent.replace(/\n/g, '<br/>');
     });
