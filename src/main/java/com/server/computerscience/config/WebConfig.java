ec80c13fba12dac6639bcafcf53ce98ff92d2608
starwook <star.wookk@gmail.com>
2024-11-07 21:40:17 +0900

    refactor: View - 해설 창 크기 조절
    - VH 적용

--- src/main/java/com/server/computerscience/config/WebConfig.java
+++ src/main/java/com/server/computerscience/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.server.computerscience.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,13 +11,5 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/robots.txt").addResourceLocations("classpath:/static/robots.txt");
 		registry.addResourceHandler("/sitemap.xml").addResourceLocations("classpath:/static/sitemap.xml");
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-			.allowedOrigins("http://localhost") // 허용할 출처 설정
-			.allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
-			.allowCredentials(true); // 자격 증명을 사용할 경우 true 설정
 	}
 }
