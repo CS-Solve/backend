ad37b05a0ebdd7c01a00aa1047d7a46de6d80547
starwook <star.wookk@gmail.com>
2024-11-08 00:31:04 +0900

    refactor: 챗봇 관련 로그인 부분과 캐시 조회 부분이 분리되도록 수정

---
+++ src/main/java/com/server/computerscience/chatbot/service/implement/ChatManageService.java
@@ -0,0 +1,51 @@
+package com.server.computerscience.chatbot.service.implement;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.server.computerscience.chatbot.domain.ChatRole;
+import com.server.computerscience.chatbot.dto.request.ChatBotRequestDto;
+import com.server.computerscience.chatbot.dto.request.ChatMessageDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ChatManageService {
+	private final String NO_MORE_CHANCE = "채팅 기회를 모두 소모하셨습니다.(1시간 마다 초기화)";
+	private final ChatCacheService chatCacheService;
+	private final ChatGptService chatGptService;
+
+	public String respond(String userId, ChatBotRequestDto chatBotRequestDto) {
+		if (!chatCacheService.canChat(userId)) {
+			return NO_MORE_CHANCE;
+		}
+		/**
+		 * User가 가지고 있는 이전 대화 기록을 가져온다.
+		 */
+		List<ChatMessageDto> chatMessages = beforeRespond(userId,
+			chatBotRequestDto);
+
+		/**
+		 * 챗봇에게 받은 답변 또한 이전 대화 기록에 넣는다.
+		 */
+		String answer = chatGptService.chat(chatMessages);
+		afterRespond(userId, answer, chatMessages);
+
+		return answer;
+	}
+
+	private List<ChatMessageDto> beforeRespond(String userId, ChatBotRequestDto chatBotRequestDto) {
+		List<ChatMessageDto> chatMessages = chatCacheService.getChatMessages(userId);
+		ChatMessageDto chatMessageFromUser = ChatMessageDto.from(chatBotRequestDto.getPrompt(), ChatRole.USER);
+		chatMessages.add(chatMessageFromUser);
+		return chatMessages;
+	}
+
+	private void afterRespond(String userId, String answer, List<ChatMessageDto> chatMessages) {
+		ChatMessageDto chatMessageFromAssistant = ChatMessageDto.from(answer, ChatRole.ASSISTANT);
+		chatMessages.add(chatMessageFromAssistant);
+		chatCacheService.increaseUsedChance(userId);
+	}
+}
--- src/main/java/com/server/computerscience/chatbot/service/implement/LoginChatBotService.java
+++ src/main/java/com/server/computerscience/chatbot/service/implement/LoginChatBotService.java
@@ -1,14 +1,10 @@
 package com.server.computerscience.chatbot.service.implement;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import com.server.computerscience.chatbot.domain.ChatRole;
 import com.server.computerscience.chatbot.dto.request.ChatBotRequestDto;
-import com.server.computerscience.chatbot.dto.request.ChatMessageDto;
 import com.server.computerscience.chatbot.service.ChatbotService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,10 +12,8 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class LoginChatBotService implements ChatbotService {
-	private final ChatGptService chatGptService;
-	private final ChatCacheService chatCacheService;
+	private final ChatManageService chatManageService;
 	private final String NOT_LOGIN = "로그인이 필요합니다.";
-	private final String NO_MORE_CHANCE = "채팅 기회를 모두 소모하셨습니다.(1시간 마다 초기화)";
 	@Value("${spring.security.oauth2.client.provider.cognito.user-name-attribute}")
 	private String USER_IDENTIFIER;
 
@@ -29,26 +23,6 @@ public class LoginChatBotService implements ChatbotService {
 			return NOT_LOGIN;
 		}
 		String userId = (String)user.getAttributes().get(USER_IDENTIFIER);
-		if (!chatCacheService.canChat(userId)) {
-			return NO_MORE_CHANCE;
-		}
-
-		/**
-		 * User가 가지고 있는 이전 대화 기록을 가져온다.
-		 */
-		List<ChatMessageDto> chatMessages = chatCacheService.getChatMessages(userId);
-		ChatMessageDto chatMessageFromUser = ChatMessageDto.from(chatBotRequestDto.getPrompt(), ChatRole.USER);
-		chatMessages.add(chatMessageFromUser);
-
-		/**
-		 * 챗봇에게 받은 답변 또한 이전 대화 기록에 넣는다.
-		 */
-		String answer = chatGptService.chat(chatMessages);
-		ChatMessageDto chatMessageFromAssistant = ChatMessageDto.from(answer, ChatRole.ASSISTANT);
-		chatMessages.add(chatMessageFromAssistant);
-
-		chatCacheService.increaseUsedChance(userId);
-
-		return answer;
+		return chatManageService.respond(userId, chatBotRequestDto);
 	}
 }
