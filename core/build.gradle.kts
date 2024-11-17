9b2a048c8e2b6a67b3635a11e95803bbaac83512
starwook <star.wookk@gmail.com>
2024-11-18 01:13:59 +0900

    build: 코틀린 모듈에 spring-tx 의존성 추가

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,10 +11,13 @@ dependencies {
 	implementation(project(":persistence"))
 
 	testImplementation(kotlin("test"))
-	kapt("org.projectlombok:lombok")
+// 	kapt("org.projectlombok:lombok")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.0")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+	// Jpa를 제외한 @Transactional만 사용
+	implementation("org.springframework:spring-tx")
 }
 
 tasks.test {
