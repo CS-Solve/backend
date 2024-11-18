4b17e32c914c38dd84d10bc02599da9bd472e7a6
starwook <star.wookk@gmail.com>
2024-11-18 13:42:09 +0900

    refactor: 자바 모듈에서 엔티티를 반환시 Nullable 표시를 하여 코틀린에서 충분히 이를 인식하도록 적용

--- persistence/src/main/java/com/comssa/persistence/member/service/MemberRepositoryService.java
+++ persistence/src/main/java/com/comssa/persistence/member/service/MemberRepositoryService.java
@@ -4,6 +4,7 @@ package com.comssa.persistence.member.service;
 import com.comssa.persistence.member.domain.Member;
 import com.comssa.persistence.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,10 +12,12 @@ import org.springframework.stereotype.Service;
 public class MemberRepositoryService {
 	private final MemberRepository memberRepository;
 
+	@Nullable
 	public Member findByCognitoId(String cognitoId) {
 		return memberRepository.findByCognitoId(cognitoId).orElse(null);
 	}
 
+	@Nullable
 	public Member findByCognitoIdFetchJoinComments(String cognitoId) {
 		return memberRepository.findByCognitoIdFetchJoinComments(cognitoId).orElse(null);
 	}
