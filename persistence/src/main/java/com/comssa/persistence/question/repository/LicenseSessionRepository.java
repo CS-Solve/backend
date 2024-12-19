a57bf2442d9a857c71fda9986d794036d668ad49
starwook <star.wookk@gmail.com>
2024-12-19 19:04:55 +0900

    build: CheckStyle 규칙 수정
    - Java Indent 코딩 컨벤션 제거

--- persistence/src/main/java/com/comssa/persistence/question/repository/LicenseSessionRepository.java
+++ persistence/src/main/java/com/comssa/persistence/question/repository/LicenseSessionRepository.java
@@ -14,12 +14,13 @@ import java.util.Optional;
 @Repository
 public interface LicenseSessionRepository extends JpaRepository<LicenseSession, Long> {
 
-    @Query(
-            "SELECT ls FROM LicenseSession ls " + "WHERE ls.content = :session AND ls.licenseCategory = :licenseCategory")
-    Optional<LicenseSession> findLicenseSessionByContent(
-            @Param("session") String content,
-            @Param("licenseCategory") LicenseCategory licenseCategory);
+	@Query(
+		"SELECT ls FROM LicenseSession ls "
+			+ "WHERE ls.content = :session AND ls.licenseCategory = :licenseCategory")
+	Optional<LicenseSession> findLicenseSessionByContent(
+		@Param("session") String content,
+		@Param("licenseCategory") LicenseCategory licenseCategory);
 
-    @Query("SELECT ls FROM LicenseSession ls WHERE ls.licenseCategory = :licenseCategory")
-    List<LicenseSession> findAllByLicenseCategory(@Param("licenseCategory") LicenseCategory licenseCategory);
+	@Query("SELECT ls FROM LicenseSession ls WHERE ls.licenseCategory = :licenseCategory")
+	List<LicenseSession> findAllByLicenseCategory(@Param("licenseCategory") LicenseCategory licenseCategory);
 }
