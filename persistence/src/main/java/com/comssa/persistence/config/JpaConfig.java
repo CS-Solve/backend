a149af4132c09b18a16bb69fb5ee1021fd0ba10b
starwook <star.wookk@gmail.com>
2024-12-20 14:43:37 +0900

    build: QueryDsl 사용을 위한 JpaQueryFactory를 빈으로 등록
    JpaConfig 클래스를 Persistence 모듈로 이동

--- persistence/src/main/java/com/comssa/persistence/config/JpaConfig.java
+++ persistence/src/main/java/com/comssa/persistence/config/JpaConfig.java
@@ -1,13 +1,26 @@
-package com.comssa.api.config;
+package com.comssa.persistence.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Configuration
 @EnableJpaAuditing
 @EntityScan(basePackages = "com.comssa.persistence")
 @EnableJpaRepositories(basePackages = "com.comssa.persistence")
 public class JpaConfig {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(entityManager);
+	}
 }
