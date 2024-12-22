5b6f55788482f77f68d1930057212fc357a0ca70
starwook <star.wookk@gmail.com>
2024-12-23 01:53:08 +0900

    refactor: QuestionQueryDslMaker가 Question에 국한되지 않도록 제네릭의 범위를 수정

--- persistence/src/main/java/com/comssa/persistence/question/repository/QueryDslJpaQueryMaker.java
+++ persistence/src/main/java/com/comssa/persistence/question/repository/QueryDslJpaQueryMaker.java
@@ -1,7 +1,6 @@
 package com.comssa.persistence.question.repository;
 
 
-import com.comssa.persistence.question.domain.common.Question;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.EntityPathBase;
 import com.querydsl.jpa.impl.JPAQuery;
@@ -9,8 +8,18 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 
+/**
+ * select 기준 테이블과 where 조건을 통해 JpaQuery를 만들어준다.
+ * EntityPathBase 제네릭을 사용함으로 특정 Q클래스에 국한되지 않고
+ * 자유롭게 Table과 where조건 문을 통해 JpaQuery를 생성할 수 있다.
+ * <p>
+ * 현재 주된 사용이유는 Qquestion, QMajorQuestion등을 조회할 때
+ * 1. 특정 테이블만 조회해야함 (select from 할 테이블을 선택할 수 있어야함)
+ * 2. 재사용하기 위함
+ * 이다.
+ */
 @RequiredArgsConstructor
-public abstract class QuestionQueryDslMaker<T extends Question> {
+public abstract class QueryDslJpaQueryMaker<T> {
 	protected final JPAQueryFactory jpaQueryFactory;
 
 
