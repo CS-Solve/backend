675cca92ac165dc281c11bc3a99dc91002cbe623
starwook <star.wookk@gmail.com>
2025-01-14 00:53:24 +0900

    refactor: 서술형 문제 채점시 프롬프트 수정

--- core/src/main/kotlin/com/comssa/core/question/service/common/DescriptiveQuestionService.kt
+++ core/src/main/kotlin/com/comssa/core/question/service/common/DescriptiveQuestionService.kt
@@ -19,7 +19,7 @@ class DescriptiveQuestionService(
 			questionGetService.getMajorDescriptiveQuestion(id)
 
 		return chatManageService.talkForGradeDescriptiveQuestion(
-			question.gradeStandard,
+			systemMessage + question.gradeStandard,
 			question.content,
 			requestDoGradeDescriptiveAnswerDto.content,
 		)
@@ -34,4 +34,15 @@ class DescriptiveQuestionService(
 		question.changeGradeStandard(requestChangeQuestionGradeStandardDto.gradeStandard)
 		return question
 	}
+
+	private val systemMessage =
+		"당신은 면접관입니다. 사용자는 면접을 보는 사람입니다. " +
+			"주어진 채점 기준에 맞추어 작성자의 답변을 점검해주세요. " +
+			"사용자의 답변에 아쉬운 부분이 있다면 과감히 말씀해주세요. " +
+			"1. 예를 들어 A에 대해 물어봤는데 답변자가 관련성이 적은 B까지 언급한다면, 지적해주셔도 됩니다.  " +
+			"2. 사용자가 틀린 내용을 말한다면 틀렸다고 말씀해주세요." +
+			"채점 기준에 사용자의 답변이 부합하지 않더라도, 채점 기준이 무조건 적인 정답이 되어선 안됩니다." +
+			"그러니 지적할 때, ~이렇게 대답하셔야합니다. 보단 ~ 이런 내용을 보충하시면 더 좋을 거같습니다. 이런 식으로 지적해주세요." +
+			"문제 채점 기준은 다음과 같습니다. 주어진 채점 기준이 없다면 당신이 임의로 판단해서 채점해주세요. " +
+			""
 }
